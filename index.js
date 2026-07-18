895a3b0349dcaa174afb980400cef0a0160f44b0
Samuel <smc14@aluno.ifnmg.edu.br>
2026-07-18 10:12:02 -0300

    feat: configura dotenv e CORS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.json({ mensagem: 'CadastraBrasil API está no ar!' }));
@@ -12,3 +15,5 @@ app.get('/status', (req, res) => res.json({ status: 'ok', timestamp: new Date() 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+export default app;
